342d88b7f10ea5aa0054faad2017150fdb33abd1
Fillip14 <fillip.apol@gmail.com>
2024-03-13 20:27:45 -0300

    Alterando a pagina com documento.body.

--- js/index.js
+++ js/index.js
@@ -3,3 +3,4 @@ const confirma = confirm("Deseja realmente apagar?"); //Confirm dentro do window
 const nome = prompt("Digite o seu nome:"); //Prompot dentro do window
 
 console.log("Alerta ", alerta, "Confirmado?", confirma, "Nome: ", nome);
+document.body.innerHTML += `Alerta: ${alerta}. Confirmou? ${confirma}. O seu nome é: ${nome}`;
