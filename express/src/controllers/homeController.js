fd8ecc8a5da7afb0012dcc9e85025cd968f2fab4
Fillip14 <fillip.apol@gmail.com>
2024-04-01 17:50:30 -0300

    Conectand com mongo, uitilizando flash mensagens e sessions

--- express/src/controllers/homeController.js
+++ express/src/controllers/homeController.js
@@ -1,4 +1,10 @@
 exports.paginaInicial = (req, res) => {
+  // req.session.usuario = { nome: "Luiz", logado: true };
+  // console.log(req.session.usuario);
+  // console.log(req.flash("info"), req.flash("error"), req.flash("success"));
+  // req.flash("info", "Ola mundo!"); //Exibe apenas uma vez, na prox atualizacao de pag n mostra mais
+  // req.flash("error", "dasd");
+  // req.flash("success", "dasdas");
   res.render("index");
 };
 
