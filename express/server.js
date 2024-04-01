99a429d4fb5c1b00e7d623d9af4a218a7357f312
Fillip14 <fillip.apol@gmail.com>
2024-04-01 17:32:16 -0300

    Realizando conexao com MongoDB Atlas

--- express/server.js
+++ express/server.js
@@ -1,5 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => app.emit("baseConnected"))
+  .catch((e) => console.log(e));
+
 const routes = require("./routes");
 const path = require("path");
 const { middlewareGlobal } = require("./src/middlewares/middleware");
@@ -14,7 +22,9 @@ app.set("view engine", "ejs");
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Acessar http://localhost:3000");
-  console.log("Servidor executando na porta 3000");
+app.on("baseConnected", () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
 });
