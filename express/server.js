d525b3b3b899077383dad680ae5cafe642090a68
Fillip14 <fillip.apol@gmail.com>
2024-04-01 16:03:28 -0300

    Utilizando routes

--- express/server.js
+++ express/server.js
@@ -1,28 +1,16 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 //CRUD - POST, GET, PUT, DEL
-
-app.get("/", (req, res) => {
-  res.send(
-    `<form action="/" method="POST">
-      Nome: <input type="text" name="nome">
-      <button> Enviar </button>
-    </form>`
-  );
-});
 
 app.get("/testes/:idUsuarios?/:parametro?", (req, res) => {
   //? no final define que nao é obrigatório ter um id
   console.log(req.params); //Pega apenas os parametros definidos
   console.log(req.query); //Pega as string depois do ?nome=Joao
   res.send(req.params);
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body); //O que é enviado no post do endereco
-  res.send(`Recebi o body da requisição: ${req.body.nome}`);
 });
 
 app.listen(3000, () => {
