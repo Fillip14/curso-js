c985cd9b34dad31bbcc482220e1ecfc56e13ee6e
Fillip14 <fillip.apol@gmail.com>
2024-04-01 16:11:04 -0300

    Criando ejs

--- express/server.js
+++ express/server.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-//CRUD - POST, GET, PUT, DEL
 
-app.get("/testes/:idUsuarios?/:parametro?", (req, res) => {
-  //? no final define que nao é obrigatório ter um id
-  console.log(req.params); //Pega apenas os parametros definidos
-  console.log(req.query); //Pega as string depois do ?nome=Joao
-  res.send(req.params);
-});
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("Acessar http://localhost:3000");
