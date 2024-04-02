1d85722625477a028c727f864f59f7bf804cedfd
Fillip14 <fillip.apol@gmail.com>
2024-04-02 10:48:29 -0300

    Adicionando helmet e CRF

--- express/server.js
+++ express/server.js
@@ -2,22 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
 mongoose
   .connect(process.env.CONNECTIONSTRING)
   .then(() => app.emit("baseConnected"))
   .catch((e) => console.log(e));
-
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
-
 const routes = require("./routes");
 const path = require("path");
-const { middlewareGlobal } = require("./src/middlewares/middleware");
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middlewares/middleware");
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, "public")));
 
 const sessionOptions = session({
@@ -30,13 +33,17 @@ const sessionOptions = session({
     httpOnly: true,
   },
 });
+
 app.use(sessionOptions);
 app.use(flash());
 
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
+app.use(csrf());
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on("baseConnected", () => {
--- express/src/middlewares/middleware.js
+++ express/src/middlewares/middleware.js
@@ -1,8 +1,23 @@
+const path = require("path");
+
 exports.middlewareGlobal = (req, res, next) => {
   res.locals.umaVariavelLocal = "Este é o valor da var local";
   if (req.body.cliente) {
     console.log();
     console.log(`Vi que voce postou ${req.body.cliente}`);
   }
+  next();
+};
+
+exports.checkCsrfError = (err, req, res, next) => {
+  if (err && err.code === "EBADCSRFTOKEN") {
+    return res.render(
+      path.resolve(__dirname, "..", "views", "includes", "404.ejs")
+    );
+  }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
   next();
 };
